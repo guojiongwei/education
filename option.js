74a0c42ead0d97e198102f232eef5093f3a3e3a6
guojiongwei <18530633992@163.com>
2020-02-29 22:26:16 +0800

    添加node服务器

---
+++ option.js
@@ -0,0 +1,29 @@
+/*
+ * @Author: your name
+ * @Date: 2019-09-01 10:42:54
+ * @LastEditTime: 2020-02-29 22:24:40
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /proxy/option.js
+ */
+const express = require('express');
+const proxy = require('http-proxy-middleware');
+const app = express();
+var history = require('connect-history-api-fallback');
+console.log(process.argv)
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+// http://bj.dingfengzhuanli.com/api/baojian/upload
+app.use('/api', proxy({
+  target: 'http://bj.dingfengzhuanli.com',
+  // pathRewrite: {'^/api' : '/'},
+  changeOrigin: true
+}))
+app.use(history());
+app.use(express.static('./'));
+// 监听端口
+app.listen('4001','0.0.0.0', () => {
+ console.log(`server running localhost:4001`);
+});
